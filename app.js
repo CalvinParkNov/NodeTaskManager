23c7f566d30f2aee113f764b62bfba257936c1fd
Won Kyun Park <wonkyunpark@Wons-MacBook-Pro.local>
2022-07-08 13:30:57 +0900

    add error page

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const app = express();
-const tasks = require('./routes/tasks')
-const connectDB = require('./db/connect')
-require('dotenv').config()
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+
 //middleware
 app.use(express.static('./public'))
 app.use(express.json());
 
-
-
 //routes
-
-// app.get('/api/v1/tasks') - gat all the tasks
-// app.post('/api/v1/tasks') - create a new task
-// app.get('/api/v1/tasks/:id') - get signle task
-// app.patch('api/v1/tasks/:id) - update task
-// app.delete('api/v1/tasks/:id') -delete Task
-
 app.use('/api/v1/tasks', tasks)
 
-
-
-
+app.use(notFound)
 
 const port = 3000
 
